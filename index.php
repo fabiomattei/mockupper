23c3d9bd5d6026b03d0513e5c55d39db529fda4f
Fabio Mattei <burattino@gmail.com>
2015-06-11 10:04:26 +0200

    updating the router to new structure

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
 
 require_once 'settings.php';
 
+// checking if I am using https
+if ( RUNNINGENVIRONMENT === 'production' AND $_SERVER['HTTPS'] === 'off' ) {
+	header( 'Location: '.BASEPATH );
+	die();
+}
+
 // remove the directory path we don't want
-$request1 = str_replace(PATHTOAPP, '', $_SERVER['REQUEST_URI']);
-$request = str_replace('.html', '', $request1);
+$request1 = substr( $_SERVER['REQUEST_URI'], strlen( PATHTOAPP ) );
+$request2  = str_replace( '.html', '', $request1 );
+$request  = str_replace( '.pdf', '', $request2 );
 
 $parameters = array();
+
 if ( strlen( $request ) > 0 ) {
 	list( $family, $subfamily, $aggregator, $parameters ) = spliturl( $request );
 
 	aggregator( $family, $subfamily, $aggregator );
 } else {
-	aggregator( 'public', '', 'index' );
+	aggregator( 'public', '', 'login' );
 }
 
 $aggregator = new Aggregator();
