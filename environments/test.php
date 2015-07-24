e069b2c7f63cf7456ffdd3ecb4f1efc682955771
Fabio Mattei <burattino@gmail.com>
2015-07-24 17:09:08 +0200

    changing style

--- environments/test.php
+++ environments/test.php
@@ -3,40 +3,40 @@
 echo 'sono in test';
 	
 // ERROR reporting
-ini_set('error_reporting', E_ALL);
+ini_set( 'error_reporting', E_ALL );
 
 // database
 /** Live environment Cloud SQL login and SITE_URL info */
 //define('DBHOST', ':/cloudsql/cihsscope:scopedb;');
-define('DBHOST', 'mysql:unix_socket=/cloudsql/cihsscope:scopedb;dbname=sat');  
-define('DBNAME', '');
-define('DBUSERNAME', 'root');
-define('DBPASSWORD', '');
+define( 'DBHOST', '' );  
+define( 'DBNAME', '' );
+define( 'DBUSERNAME', 'root' );
+define( 'DBPASSWORD', '' );
 
 /* 
  * APPLICATION BASE PATH
  * BASEPATH: the base URL where the web application is installed
  * PATHTOAPP: it is the eventual subfolder where the application is installed
  */
-define('BASEPATH', 'http://www.example.com/');
-define('PATHTOAPP', '/mockupper/');
+define( 'BASEPATH', 'http://www.example.com/' );
+define( 'PATHTOAPP', '/mockupper/' );
 
 /* 
  * PREDIFINED LOCATOR
  * here you can set the file open by default when a general user is visiting the 
  * base URL of the system
  */
-define('FAMILY',     'public');
-define('SUBFAMILY',  '');
-define('AGGREGATOR', 'index');
+define( 'FAMILY',     'public' );
+define( 'SUBFAMILY',  '' );
+define( 'AGGREGATOR', 'index' );
 
 /* 
  * PREDIFINED TEMPLATES FILES
  * PUBLICTEMPLATE: filename of public template contained in the "templates" folder without .php 
  * PRIVATETEMPLATE:  filename of private template contained in the "templates" folder without .php 
  */
-define('PUBLICTEMPLATE',  'public');
-define('PRIVATETEMPLATE', 'application');
+define( 'PUBLICTEMPLATE',  'public' );
+define( 'PRIVATETEMPLATE', 'application' );
 
 // TIMEZONE
-date_default_timezone_set('Europe/Rome');
+date_default_timezone_set( 'Europe/Rome' );
